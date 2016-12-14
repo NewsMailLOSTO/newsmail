89b5419ed460337c52cee540eb776e007e6da225
uczen <uczen@Pracownia1Pc01>
2016-12-14 12:47:21 +0100

    duppa

--- rejestracja.php
+++ rejestracja.php
@@ -7,16 +7,19 @@
 
 
 <form action="dodaj.dodaj.php" method="post" name="form1" id="form1" onsubmit="return sprawdz_formularz()">
-    <fieldset> <p>Imię: <input type='text' name='imie' id='imie' placeholder='Imię' maxlength="16"/></p>
-            <p>Nazwisko: <input type='text' name='nazwisko' id='nazwisko' placeholder='Nazwisko' maxlength="32"/></p>
-            <p>Email: <input type='text' name='email' id='email' placeholder='Email' maxlength="64"/></p>
-            <p>Hasło: <input type='password' name='haslo' id='haslo' placeholder='Hasło' maxlength="64"/></p>
+    
+    <table class="kupa" border=1px><tr><td>Imię: </td><td> <input type='text' name='imie' id='imie' placeholder='Imię' maxlength="16"/></td></tr>
+    <tr> <td> Nazwisko:</td><td> <input type='text' name='nazwisko' id='nazwisko' placeholder='Nazwisko' maxlength="32"/> </td></tr>
+     <tr><td>   Email:</td><td> <input type='text' name='email' id='email' placeholder='Email' maxlength="64"/></td></tr>
+    <tr> <td>      Hasło:</td><td> <input type='password' name='haslo' id='haslo' placeholder='Hasło' maxlength="64"/></td></tr>
             
-            
+    </table>
+        
             <br />
             <br />
+            <div id="rej-button">
             <button type='submit' name='btn-upload'>Rejestruj</button>
-    </fieldset>
+                <div/>
         </form>
 
  <script type="text/javascript" src="js/formy.js"></script>
