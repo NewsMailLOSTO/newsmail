b7db193ba49d11921c41c720aeac07ac38623a72
uczen <uczen@Pracownia1Pc01>
2016-12-14 12:01:48 +0100

    ramka dla rejestracji

--- rejestracja.php
+++ rejestracja.php
@@ -7,7 +7,7 @@
 
 
 <form action="dodaj.dodaj.php" method="post" name="form1" id="form1" onsubmit="return sprawdz_formularz()">
-            <p>Imię: <input type='text' name='imie' id='imie' placeholder='Imię' maxlength="16"/></p>
+    <fieldset> <p>Imię: <input type='text' name='imie' id='imie' placeholder='Imię' maxlength="16"/></p>
             <p>Nazwisko: <input type='text' name='nazwisko' id='nazwisko' placeholder='Nazwisko' maxlength="32"/></p>
             <p>Email: <input type='text' name='email' id='email' placeholder='Email' maxlength="64"/></p>
             <p>Hasło: <input type='password' name='haslo' id='haslo' placeholder='Hasło' maxlength="64"/></p>
@@ -16,7 +16,7 @@
             <br />
             <br />
             <button type='submit' name='btn-upload'>Rejestruj</button>
-            
+    </fieldset>
         </form>
 
  <script type="text/javascript" src="js/formy.js"></script>
