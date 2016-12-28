a4a97d0ac2e0cee00712bb3b2fe26bf83002ef46
sarian <sarian@sarian-xubuntu>
2016-12-28 15:54:09 +0100

    sezja

--- rejestracja.php
+++ rejestracja.php
@@ -16,7 +16,7 @@ $odbiorca = "email";
 
 <form action="dodaj.dodaj.php" method="post" name="form1" id="form1" onsubmit="return sprawdz_formularz();">
     
-    <table class="kupa" border=1px><tr><td>Imię: </td><td> <input type='text' name='imie' id='imie' placeholder='Imię' maxlength="16"/></td></tr>
+    <table class="kupa"><tr><td>Imię: </td><td> <input type='text' name='imie' id='imie' placeholder='Imię' maxlength="16"/></td></tr>
     <tr> <td> Nazwisko:</td><td> <input type='text' name='nazwisko' id='nazwisko' placeholder='Nazwisko' maxlength="32"/> </td></tr>
      <tr><td>   Email:</td><td> <input type='text' name='email' id='email' placeholder='Email' maxlength="64"/></td></tr>
     <tr> <td>      Hasło:</td><td> <input type='password' name='haslo' id='haslo' placeholder='Hasło' maxlength="64"/></td></tr>
---
+++ session.php
@@ -0,0 +1,20 @@
+<?php
+include("db.php");
+
+session_start(); 
+
+$email=($_POST['email']);
+$hasło=($_POST['hasło']);
+
+$result=mysql_query("SELECT count(*) FROM student WHERE email='$email' and haslo='$hasło'");
+
+$count=mysql_fetch_array($result);
+
+if($count==0){
+  session_register("email");
+  session_register("hasło");
+  header("location:index.php");
+} else {
+  echo 'Wrong Email or Password! Return to <a href="index.html">login</a>';
+  }
+?>
